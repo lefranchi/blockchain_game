b22aa795507e8cd624482ef7afadab79ffa272fe
Leandro Franchi <leandro.franchi@gmail.com>
2022-01-18 22:50:12 -0300

    Some tests.

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.5.0;
 
 import "./ERC721Full.sol";
 
-//contract MemoryToken is ERC721Full {
-contract MemoryToken {
-  string public name = "Memory Token";
+contract MemoryToken is ERC721Full {
+  //string public name = "Memory Token";
+
+  constructor() ERC721Full("Memory Token", "MEMORY") public {
+  }
 
 }
